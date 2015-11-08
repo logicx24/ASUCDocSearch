3bd2e3a7cdbd2e1836cd7ab3b8a12c9832b2846a
Aakash Japi <aakashjapi@gmail.com>
2015-11-07 18:19:51 -0800

    google drive fucking sucks

--- drivePipeline.py
+++ drivePipeline.py
@@ -8,9 +8,10 @@ from oauth2client import client
 from oauth2client import tools
 import json
 
-
+asucFolderId = "0B7_j_4L-LvIWd0pIaldkVFBMRDg"
 
 def init():
+
 	with open("service_account_creds.json") as creds:
 		credJson = json.load(creds)
 
@@ -18,3 +19,23 @@ def init():
 	authedHttp = httplib2.Http()
 	credentials.authorize(authedHttp)
 	return discovery.build('drive', 'v2', http=authedHttp)
+
+
+def printFilesInFolder(service, folder_id):
+  page_token = None
+  while True:
+    try:
+      param = {}
+      if page_token:
+        param['pageToken'] = page_token
+      children = service.children().list(
+          folderId=folder_id, **param).execute()
+
+      for child in children.get('items', []):
+        print(child)
+      page_token = children.get('nextPageToken')
+      if not page_token:
+        break
+    except errors.HttpError, error:
+      print ("An error occurred:")
+      break
